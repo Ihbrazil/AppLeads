fcea7798f8dc4b96e2a27521ca0645f5bdcd77b6
ihbrazil <ihbrazil@users.noreply.github.com>
2024-09-25 21:16:56 -0300

    ValidacaoDeDados
    Validação de dados por meio de expressões regulares que são fornecidos pelo Front-end

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 
 import { cadastraLead } from './servico/cadastroServico.js';
+import { validaUsuario } from './validacao/valida.js';
 
 const app = express();
 
@@ -12,9 +13,15 @@ app.post("/usuarios", async (req, res) => {
     const nome = req.body.nome;
     const email = req.body.email;
 
-    await cadastraLead(nome, email);
+    const usuarioValido = validaUsuario(nome, email);
 
-    res.status(204).end();
+    if (usuarioValido.status) {
+        await cadastraLead(nome, email);
+        res.status(204).end();
+    } else {
+        res.status(400).send({mensagem: usuarioValido.mensagem});
+    }
+    
 });
 
 app.listen(3001, async () => {
